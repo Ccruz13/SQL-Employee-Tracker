31f749d00e55f712b81fa3b0b2d98329322910b2
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2021-11-14 17:08:38 -0500

    added functions to server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const console = require("console.table");
+const ctable = require("console.table");
 
 const connection = mysql.createConnection({
+    multipleStatements: true,
     host: "localhost",
     port: 3306,
     user: "root",
@@ -19,7 +20,7 @@ connection.connect(function (err) {
 
 // first prompt of program
 const promptUser = () => {
-    return inquirer.prompt ([
+    return inquirer.prompt([
         {
             type: "list",
             name: "first",
@@ -35,25 +36,25 @@ const promptUser = () => {
             ]
         }
     ])
-    .then((answers) => {
-        const {choices} = answers;
+        .then((answers) => {
+            const { choices } = answers;
 
-        if (choices === "View All Departments") {
-            return viewAllDepartments();
-        } else if (choices === "View All roles") {
-            return viewAllRoles();
-        } else if (choices === "View All Employees") {
-            return viewAllEmployees();
-        } else if (choices === "Add a department") {
-            return addDepartment();
-        } else if (choices === "Add a Role") {
-            return addRole();
-        } else if (choices === "Add an Employee") {
-            return addEmployee();
-        } else if (choices === "Update Employee role") {
-            return updateEmployeeRole();
-        }
-    });
+            if (choices === "View All Departments") {
+                return viewAllDepartments();
+            } else if (choices === "View All roles") {
+                return viewAllRoles();
+            } else if (choices === "View All Employees") {
+                return viewAllEmployees();
+            } else if (choices === "Add a department") {
+                return addDepartment();
+            } else if (choices === "Add a Role") {
+                return addRole();
+            } else if (choices === "Add an Employee") {
+                return addEmployee();
+            } else if (choices === "Update Employee role") {
+                return updateEmployeeRole();
+            }
+        });
 };
 
 //View Departments
@@ -72,7 +73,7 @@ const viewAllRoles = () => {
                 INNER JOIN department ON role.department_id = department.id`;
     connection.promise().query(sql, (err, res) => {
         if (err) throw (err);
-        response.forEach((role) => {
+        res.forEach((role) => {
             console.log(role.title);
         });
         promptUser();
@@ -94,5 +95,104 @@ const viewAllEmployees = () => {
     connection.promise().query(sql, (err, res) => {
         if (err) throw (err);
         console.table(res);
+    });
+};
+
+const addDepartment = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'newDepartment',
+            message: 'What is the name of the new department?',
+            validate: newDepartmentInput => {
+                if (newDepartmentInput) {
+                    return true;
+                } else {
+                    console.log("Please enter new department name!");
+                    return false;
+                }
+            }
+        }
+    ])
+        .then((answer) => {
+            let sql = `INSERT INTO department (department_name) VALUES (?)`;
+            connection.query(sql, answer.newDepartment, (err, res) => {
+                if (err) throw (err);
+                console.log(answer.newDepartment + `department has been created!`);
+                viewAllDepartments();
+            });
+        });
+};
+
+const addRole = () => {
+    const sql = 'SELECT * FROM department'
+    connection.promise().query(sql, (err, res) => {
+        if (err) throw (err);
+        let depNameArray = [];
+        res.forEach((department) => { depNameArray.push(department.department_name); });
+        depNameArray.push('Create Department');
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'departmentName',
+                message: 'Which department is this new role in?',
+                choices: depNameArray
+            }
+        ])
+            .then((answer) => {
+                if (answer.departmentName === 'Create Department') {
+                    this.addDepartment();
+                } else {
+                    addRoleQues(answer);
+                }
+            });
+
+        const addRoleQues = (departmentData) => {
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'newRole',
+                    message: 'What is the name of the new role?',
+                    validate: newRoleInput => {
+                        if (newRoleInput) {
+                            return true;
+                        } else {
+                            console.log("Please enter new role name!");
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: 'What is the salary of the new role?',
+                    validate: salaryInput => {
+                        if (salaryInput) {
+                            return true;
+                        } else {
+                            console.log("Please enter a salary.");
+                            return false;
+                        }
+                    }
+                }
+            ])
+                .then((answer) => {
+                    let createdRole = answer.newRole;
+                    let departmentId;
+
+                    res.forEach(((department) => {
+                        if (departmentData.departmentName === department.department_name) { departmentId = department.id };
+                    }));
+
+                    let sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+                    let crit = [createdRole, answer.salary, departmentId];
+
+                    connection.promise().query(sql, crit, (err) => {
+                        if (err) throw (err);
+                        console.log('Role created');
+                        viewAllRoles();
+                    });
+                });
+        };
     });
 };
